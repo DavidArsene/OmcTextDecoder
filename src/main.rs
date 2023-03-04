e6aa57abffef912c02ac79805cb13c73323a1c88
DavidArsene <80218600+DavidArsene@users.noreply.github.com>
2023-03-04 21:33:56 +0200

    Add code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![allow(non_upper_case_globals)]
+
+use std::io::Result;
+use std::fs::read_dir;
+use std::path::Path;
+
+mod omctextdecoder;
+
 fn main() {
-	println!("Hello, world!");
+	let from = std::env::args().nth(1).unwrap_or(".".into());
+	if let Err(err) = walk_dir(from.as_ref()) {
+		eprintln!("{err}");
+	}
+}
+
+fn walk_dir(from: &Path) -> Result<()> {
+	for entry in read_dir(from)? {
+		let path = entry?.path();
+
+		if path.is_dir() {
+			walk_dir(&path)?;
+		} else {
+			omctextdecoder::decode(&path)?;
+		}
+	}
+	Ok(())
 }
